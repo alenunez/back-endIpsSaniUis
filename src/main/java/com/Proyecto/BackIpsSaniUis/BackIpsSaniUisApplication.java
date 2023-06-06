d666abb0c8d3fcc876cda1497a19671ca41d29a3
Alejandro Nuñez Herrera <alejandro2191947@correo.uis.edu.co>
2023-06-06 11:16:27 -0500

    Creación del proyecto

---
+++ src/main/java/com/Proyecto/BackIpsSaniUis/BackIpsSaniUisApplication.java
@@ -0,0 +1,13 @@
+package com.Proyecto.BackIpsSaniUis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackIpsSaniUisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackIpsSaniUisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Proyecto/BackIpsSaniUis/BackIpsSaniUisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto.BackIpsSaniUis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackIpsSaniUisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
