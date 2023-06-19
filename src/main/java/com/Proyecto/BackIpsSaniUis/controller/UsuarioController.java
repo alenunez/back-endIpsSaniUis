ac1983fdfe5609c39db55bb449babfd9c5e3a633
Alejandro Nuñez Herrera <alejandro2191947@correo.uis.edu.co>
2023-06-19 17:39:10 -0500

    Creación de metodo post para crear usuarios

--- src/main/java/com/Proyecto/BackIpsSaniUis/controller/UsuarioController.java
+++ src/main/java/com/Proyecto/BackIpsSaniUis/controller/UsuarioController.java
@@ -1,5 +1,40 @@
 package com.Proyecto.BackIpsSaniUis.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Proyecto.BackIpsSaniUis.dto.UsuarioDTO;
+import com.Proyecto.BackIpsSaniUis.mappers.UsuarioMappersImpl;
+import com.Proyecto.BackIpsSaniUis.model.Usuario;
+import com.Proyecto.BackIpsSaniUis.repository.UsuarioRepository;
+import com.Proyecto.BackIpsSaniUis.service.interfaces.IUsuarioService;
+
+@RestController
+@RequestMapping("/usuario")
 public class UsuarioController {
+
+  IUsuarioService iUsuarioService;
+
+  @PostMapping("/insert")
+  public ResponseEntity<Usuario> createUsuario(@RequestBody UsuarioDTO usuarioDTO){
+
+
+    Usuario usuario = UsuarioMappersImpl.INSTANCE.usuarioDTOToUsuario(usuarioDTO);
+    iUsuarioService.createUsuario(usuario);
+
+    return new ResponseEntity<>(usuario, HttpStatus.CREATED);
+    
+  }
+
+  @Autowired
+  public UsuarioController(IUsuarioService iUsuarioService) {
+      this.iUsuarioService =iUsuarioService;
+  }
+
     
 }
--- src/main/java/com/Proyecto/BackIpsSaniUis/service/interfaces/IUsuarioService.java
+++ src/main/java/com/Proyecto/BackIpsSaniUis/service/interfaces/IUsuarioService.java
@@ -1,5 +1,13 @@
 package com.Proyecto.BackIpsSaniUis.service.interfaces;
 
-public class IUsuarioService {
-    
+import org.springframework.stereotype.Service;
+
+import com.Proyecto.BackIpsSaniUis.model.Usuario;
+
+@Service
+public interface IUsuarioService {
+
+	void createUsuario(  Usuario usuarioDTO);
+
 }
+
